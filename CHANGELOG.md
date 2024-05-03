f88ab74c9450f1a25556aaa49e98a43cf2f15bc0
14H <do.duc.hieu@linecorp.com>
2024-05-03 20:11:10 +0700

    Create release v 0.0.1 for module bip

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# CHANGELOG
+
+## [0.0.1] - Release Date
+
+#### bip39
+
+- Initial release of the `bip39` module.
+
+#### bip44
+
+- Initial release of the `bip44` module.
+
+#### crypto
+
+- Initial release of the `crypto` module. This module includes implementation of kotlin crypto hash.
--- crypto/build.gradle.kts
+++ crypto/build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     id(libs.plugins.commonMppLib.get().pluginId)
+    id(libs.plugins.commonMppPublish.get().pluginId)
 }
+
+publishConfig {
+    url = "https://maven.pkg.github.com/KryptonReborn/kotlin-bip"
+    groupId = "kotlin-bip"
+    artifactId = "crypto"
+}
+
+version = "0.0.1"
 
 android {
     namespace = "dev.kryptonreborn.bip.crypto"
